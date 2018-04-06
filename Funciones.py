c093324414c85ed6111d0d9066867381afffc2c8
OlgaCobaquil <olga.cobaquil94@gmail.com>
2018-04-05 18:45:53 -0600

    Initial commit

---
+++ Funciones.py
@@ -0,0 +1,3 @@
+def abrir_archivo():
+    f = open("Input.txt", "r")  # opens file with name of "test.txt"
+    print(f.read())
---
+++ Main.py
@@ -0,0 +1,6 @@
+""" Proyecto 2 Task 2 Inteligencia artificial """
+import Funciones as fnc
+
+fnc.abrir_archivo();
+
+
